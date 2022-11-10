2ab14b8eda511b31fe343fd12465fd2be2d363b0
Manuel Espinoza <mespinoq@uc.cl>
2022-11-10 20:09:06 -0300

    Consulta 1

--- Sites/consultas/consulta1.php
+++ Sites/consultas/consulta1.php
@@ -1,7 +1,14 @@
 <?php
 //Entregue un listado del nombre y tel´efono de contacto de todos los artistas
 if($boton_1) {
-    include "../config/conexion.php";
+    $user = 'grupo31';
+    $password = 'basesdedatos31';
+    $databaseName = 'grupo31e2';
+    try {
+        $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+        } catch (Exception $e) {
+        echo "No se pudo conectar a la base de datos: $e";
+    }
     $query = "SELECT nombre_escenico, contacto FROM artistas;";
     $result = $db -> prepare($query);
     $result -> execute();
--- Sites/consultas/consulta2.php
+++ Sites/consultas/consulta2.php
@@ -1,13 +1,33 @@
 <?php
 //Dado un artista, entregue el n´umero de entradas de cortes´ıa que ha entregado
 if($boton_2) {
-    foreach (range(0, 2) as $number) {
-        echo '<tr>
-        <th scope="row">'.$number.'</th>
-        <td><?php echo $artista ?></td>
-        <td><?php echo $artista ?></td>
-        <td><?php echo $artista ?></td>
-      </tr>';
+    $user = 'grupo31';
+    $password = 'basesdedatos31';
+    $databaseName = 'grupo31e2';
+    try {
+        $db = new PDO("pgsql:dbname=$databaseName;host=localhost;port=5432;user=$user;password=$password");
+        } catch (Exception $e) {
+        echo "No se pudo conectar a la base de datos: $e";
     }
-}
+    $query = "";
+    $result = $db -> prepare($query);
+    $result -> execute();
+    $dataCollected = $result -> fetchAll();
+    echo '
+        <thead>
+        <tr>
+            <th scope="col">Nombre escenico</th>
+            <th scope="col">Contacto</th>
+        </tr>
+        </thead>
+        <tbody>';
+
+        foreach ($dataCollected as $p) {
+            echo "<tr>
+                <td>$p[0]</td>
+                <td>$p[1]</td>
+                </tr>";
+        };
+        echo '</tbody>';
+    }
 ?>
