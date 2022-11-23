37ed7686785f14789ddd5128f826003d1be49768
Manuel Espinoza <mespinoq@uc.cl>
2022-11-23 19:07:47 -0300

    Update Sites for E3

--- Sites/config/data.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $user = 'grupo31';
-  $password = 'basesdedatos31';
-  $databaseName = 'grupo31e2';
-?>
--- Sites/templates/footer.php
+++
@@ -1,5 +0,0 @@
-<footer class="footer fixed-bottom text-center text-lg-start bg-light ">
-<div class="text-center p-4"">
-    <h6>Gerardo Alcalde / Manuel Espinoza</h6>
-  </div>
-</footer>
