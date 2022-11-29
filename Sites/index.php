9b3d83f490a6b4eb035d60aa1a73158d6b20a07d
Manuel Espinoza <mespinoq@uc.cl>
2022-11-29 02:31:12 -0300

    Login design :white_check_mark:

--- Sites/index.php
+++ Sites/index.php
@@ -1,1 +1,58 @@
-<h1>Index E3</h1>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <title>E3 Grupo 31-32</title>
+    </head>
+    <body>
+    <section class="vh-100">
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col-sm-6 text-black">
+            <br>
+            <div class="px-5 ms-xl-4">
+            <span class="h1 fw-bold mb-0">Entrega 3</span>
+            </div>
+
+            <div class="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
+
+            <form style="width: 23rem;">
+
+                <h3 class="fw-normal mb-3 pb-3" style="letter-spacing: 1px;">Ingrese sus datos:</h3>
+
+                <div class="form-outline mb-4">
+                <label class="form-label" for="form2Example18">Correo</label>
+                <input type="email" id="form2Example18" class="form-control form-control-lg" />
+                </div>
+
+                <div class="form-outline mb-4">
+                <label class="form-label" for="form2Example28">Contraseña</label>
+                <input type="password" id="form2Example28" class="form-control form-control-lg" />
+                </div>
+
+                <div class="pt-1 mb-4">
+                <button class="btn btn-outline-info" type="button">Ingresar</button>
+                </div>
+
+                <p>¿No tienes cuenta? <a href="#!" class="link-info">Registrarse</a></p>
+
+            </form>
+            <div class="position-absolute bottom-0 start-10">
+                <p> &#128512; Hecho por Grupos 31-32 2022-02</p>
+            </div>
+        </div>
+
+        </div>
+        <div class="col-sm-6 px-0 d-none d-sm-block">
+            <img src="https://i.pinimg.com/originals/a9/10/bb/a910bb5c898d242bea6b167c30ef6e07.jpg"
+            alt="Login image" class="w-100 vh-100" style="object-fit: cover; object-position: left;">
+        </div>
+        </div>
+    </div>
+    </section>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    </body>
+</html>
