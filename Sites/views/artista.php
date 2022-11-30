77a2cf026c5116fcb679b0e4c967965f48af24e9
Manuel Espinoza <mespinoq@uc.cl>
2022-11-30 19:21:07 -0300

    Logout

--- Sites/views/artista.php
+++ Sites/views/artista.php
@@ -1,1 +1,5 @@
 <h1>Bienvenido/a: <?php echo $_SESSION['username'] ?> Artista</h1>
+
+<form action="views/logout.php" method="get">
+    <input type="submit" value="Cerrar sesion" class="close-button">
+</form>
--- Sites/views/productora.php
+++ Sites/views/productora.php
@@ -1,1 +1,6 @@
 <h1>Bienvenido/a: <?php echo $_SESSION['username'] ?> Productora</h1>
+
+
+<form action="views/logout.php" method="get">
+    <input type="submit" value="Cerrar sesion" class="close-button">
+</form>
