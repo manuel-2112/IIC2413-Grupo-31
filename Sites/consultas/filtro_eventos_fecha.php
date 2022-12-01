4bad9ab33ce2b8e256aca71a30ded143258bff2b
Manuel Espinoza <mespinoq@uc.cl>
2022-12-01 17:07:41 -0300

    arreglo

--- Sites/consultas/filtro_eventos_fecha.php
+++ Sites/consultas/filtro_eventos_fecha.php
@@ -11,8 +11,8 @@
     echo $fecha_inicio;
 
  	$query = "SELECT E.nombre as nombre FROM Evento as E, Productoras as P, ProductoraEvento as PE WHERE P.id_p = PE.id_p AND E.id_ev = PE.id_ev AND LOWER(P.nombre) = '$name' AND E.fecha_inicio >= '$fecha_inicio' AND E.fecha_termino <= '$fecha_termino';";
-	$result = $db2 -> prepare($query);
-	$result -> execute();
+	$result_fechas = $db2 -> prepare($query);
+	$result_fechas -> execute();
 	$productoras = $result_fechas -> fetchAll();
   header('Location: ../index.php');
 ?>
