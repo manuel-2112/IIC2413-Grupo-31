d69c57f823778e1b33935e3851b6d8ff3de83aa0
Felipe Aguirre <felipe.aguirre@uc.cl>
2022-12-01 16:29:34 -0300

    filtro

--- Sites/consultas/filtro_eventos_fecha.php
+++ Sites/consultas/filtro_eventos_fecha.php
@@ -8,6 +8,7 @@
     require("../config/connection.php");
     $fecha_inicio = $_POST["fecha_inicio"];
     $fecha_termino = $_POST["fecha_termino"];
+    echo $fecha_inicio;
 
  	$query = "SELECT E.nombre as nombre FROM Evento as E, Productoras as P, ProductoraEvento as PE WHERE P.id_p = PE.id_p AND E.id_ev = PE.id_ev AND P.nombre = 'LetsGo Company' AND E.fecha_inicio >= $fecha_inicio AND E.fecha_termino <= $fecha_termino;";
 	$result = $db2 -> prepare($query);
