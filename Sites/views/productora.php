d55084e67f7a8a97c6153cc6d450a4eb8f974837
Manuel Espinoza <mespinoq@uc.cl>
2022-12-01 15:54:22 -0300

    arreglo

--- Sites/views/productora.php
+++ Sites/views/productora.php
@@ -6,16 +6,7 @@
 </form>
 
 <?php
-  $name = str_replace('_',' ',$_SESSION['username']);
-  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/connection.php");
-
- 	$query = "SELECT E.nombre as nombre, E.fecha_inicio as fecha_inicio, E.fecha_termino
-     FROM Evento as E, Productoras as P, ProductoraEvento as PE
-     WHERE P.id_p = PE.id_p AND E.id_ev = PE.id_ev AND LOWER(P.nombre) = '$name'";
-	$result = $db2 -> prepare($query);
-	$result -> execute();
-	$productoras = $result -> fetchAll();
+  include('../consultas/eventos_productora.php')
   ?>
 
 	<table align="center">
