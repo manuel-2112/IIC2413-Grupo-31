3dd335aefcaf2852fd371b0c0915632e1f174677
Manuel Espinoza <mespinoq@uc.cl>
2022-12-01 15:42:39 -0300

    productora

--- Sites/views/productora.php
+++ Sites/views/productora.php
@@ -13,7 +13,7 @@
  	$query = "SELECT E.nombre as nombre, E.fecha_inicio as fecha_inicio, E.fecha_termino
      FROM Evento as E, Productoras as P, ProductoraEvento as PE
      WHERE P.id_p = PE.id_p AND E.id_ev = PE.id_ev AND LOWER(P.nombre) = '$name'";
-	$result = $db -> prepare($query);
+	$result = $db2 -> prepare($query);
 	$result -> execute();
 	$productoras = $result -> fetchAll();
   ?>
