f9af968705865f4f119a8a1706367d2e21209757
Manuel Espinoza <mespinoq@uc.cl>
2022-12-02 18:51:18 -0300

    Funcion SQL

--- Sites/consultas/importar_usuarios.php
+++ Sites/consultas/importar_usuarios.php
@@ -10,6 +10,7 @@
         $result_productoras = $db2 -> prepare($query_productoras);
         $result_productoras -> execute();
         $data_productoras = $result_productoras -> fetchAll();
+        echo(print_r($data_productoras));
 
         foreach ($data_productoras as $productora){
             $query_copia = "SELECT insertar_productora('$productora[0]'::varchar, '$productora[1]'::varchar, '$productora[2]'::varchar)";
