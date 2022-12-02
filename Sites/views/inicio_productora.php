65457ac658b4e886af1a1651bc30d48f79e936f4
Manuel Espinoza <mespinoq@uc.cl>
2022-12-02 11:36:56 -0300

    Productora dashboard

--- Sites/views/inicio_productora.php
+++ Sites/views/inicio_productora.php
@@ -1,63 +1,95 @@
 <div class="row">
 <div class="col-xl-3 col-md-6">
     <div class="card bg-info text-white mb-4">
-        <div class="card-body">Consulta 1</div>
-        <div class="card-footer d-flex align-items-center justify-content-between">
-            <a class="small text-white stretched-link" data-bs-toggle="collapse" href="#detalles1" aria-expanded="false" aria-controls="detalles1">Detalles</a>
+        <div class="card-body">
+            <a href="#"><div class="d-flex justify-content-between">
+            <p>Consulta 1</p>
             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            </div></a>
+        </div>
+        
+        <div class="card-footer d-grid">
+            <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#detalles1">
+                Detalles
+                </button>
         </div>
     </div>
+</div>
 </div>
 <div class="col-xl-3 col-md-6">
     <div class="card bg-info text-white mb-4">
-        <div class="card-body">Consulta 2</div>
-        <div class="card-footer d-flex align-items-center justify-content-between">
-        <a class="small text-white stretched-link" data-bs-toggle="collapse" href="#detalles2" aria-expanded="false" aria-controls="detalles2">Detalles</a>
+        <div class="card-body">
+            <a href="#"><div class="d-flex justify-content-between">
+            <p>Consulta 2</p>
             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            </div></a>
+        </div>
+        
+        <div class="card-footer d-grid">
+            <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#detalles2">
+                Detalles
+                </button>
         </div>
     </div>
+</div>
 </div>
 <div class="col-xl-3 col-md-6">
     <div class="card bg-info text-white mb-4">
-        <div class="card-body">Consulta 3</div>
-        <div class="card-footer d-flex align-items-center justify-content-between">
-        <a class="small text-white stretched-link" data-bs-toggle="collapse" href="#detalles3" aria-expanded="false" aria-controls="detalles3">Detalles</a>
+        <div class="card-body">
+            <a href="#"><div class="d-flex justify-content-between">
+            <p>Consulta 3</p>
             <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+            </div></a>
+        </div>
+        
+        <div class="card-footer d-grid">
+            <button type="button" class="btn btn-outline-light" data-bs-toggle="modal" data-bs-target="#detalles3">
+                Detalles
+                </button>
         </div>
     </div>
 </div>
-<div class="col-xl-3 col-md-6">
-    <div class="card bg-info text-white mb-4">
-        <div class="card-body">Consulta 4</div>
-        <div class="card-footer d-flex align-items-center justify-content-between">
-        <a class="small text-white stretched-link" data-bs-toggle="collapse" href="#detalles4" aria-expanded="false" aria-controls="detalles4">Detalles</a>
-            <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+</div>
+
+
+<div class="modal fade" id="detalles1" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalles Consulta 1</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Aca va la explicacion de la consulta
+        </div>
         </div>
     </div>
 </div>
-</div>
-</div>
 
-<div class="collapse" id="detalles1">
-    <div class="card card-body">
-        Aca va a salir una explicacion de que hace la consulta.
+<div class="modal fade" id="detalles2" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalles Consulta 2</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Aca va la explicacion de la consulta
+        </div>
+        </div>
     </div>
 </div>
 
-<div class="collapse" id="detalles2">
-    <div class="card card-body">
-        Aca va a salir una explicacion de que hace la consulta.
-    </div>
-</div>
-
-<div class="collapse" id="detalles3">
-    <div class="card card-body">
-        Aca va a salir una explicacion de que hace la consulta.
-    </div>
-</div>
-
-<div class="collapse" id="detalles4">
-    <div class="card card-body">
-        Aca va a salir una explicacion de que hace la consulta.
+<div class="modal fade" id="detalles3" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">Detalles Consulta 3</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            Aca va la explicacion de la consulta
+        </div>
+        </div>
     </div>
 </div>
