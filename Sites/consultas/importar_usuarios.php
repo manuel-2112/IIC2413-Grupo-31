fe40bbbb0e94136ae64368004ecd9f135f356ec3
Manuel Espinoza <mespinoq@uc.cl>
2022-12-02 17:35:21 -0300

    Funcion SQL

--- Sites/consultas/importar_usuarios.php
+++ Sites/consultas/importar_usuarios.php
@@ -5,13 +5,18 @@
         $result = $db2 -> prepare($query);
         $result -> execute();
         $data = $result -> fetchAll();
-        echo(print_r($data));
+
+        $query_artistas = "SELECT importar_artistas();";
+        $result_artistas = $db1 -> prepare($query_artistas);
+        $result_artistas -> execute();
+        $data_artistas = $result_artistas -> fetchAll();
         
         $query1 = "SELECT * FROM usuarios;";
         $result1 = $db2 -> prepare($query1);
         $result1 -> execute();
         $data1 = $result1 -> fetchAll();
-        echo(print_r($data1))
+        echo(print_r($data1));
+        echo(print_r($data_artistas))
     ?>
 
 <?php include('../templates/footer.php') ?>
