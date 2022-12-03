d541d2f95a9e399be75c715f90fd65dab7726238
Manuel Espinoza <mespinoq@uc.cl>
2022-12-02 22:06:08 -0300

    djbkja

--- Sites/consultas/importar_usuarios.php
+++ Sites/consultas/importar_usuarios.php
@@ -1,4 +1,14 @@
-<?php include('../templates/header.php'); ?>    
+<?php include('../templates/header.php'); ?>  
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="../css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+</head>
     <?php
         require("../config/connection.php");
         $query_tabla = "SELECT crear_tabla_usuarios();";
@@ -71,5 +81,6 @@
             </div>
         </div>
     </div>
+</div>
 
 <?php include('../templates/footer.php') ?>
