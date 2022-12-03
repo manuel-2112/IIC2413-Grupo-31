c21b305b284690c9e3d79edceea3cc9e941b4fd3
Manuel Espinoza <mespinoq@uc.cl>
2022-12-02 21:44:21 -0300

    djbkja

--- Sites/consultas/importar_usuarios.php
+++ Sites/consultas/importar_usuarios.php
@@ -16,8 +16,6 @@
         $result_usuarios -> execute();
         $data_usuarios = $result_usuarios -> fetchAll();
 
-        echo(print_r($data_usuarios));
-
         foreach ($data_usuarios as $productora){
             $query_copia = "SELECT insertar_productora('$productora[1]'::varchar, '$productora[2]'::varchar, '$productora[3]'::varchar)";
             $result_copia = $db -> prepare($query_copia);
@@ -34,7 +32,42 @@
         $result1 = $db -> prepare($query1);
         $result1 -> execute();
         $data1 = $result1 -> fetchAll();
-        echo(print_r($data1));
     ?>
+    <div id="container">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Usuarios</h1><br>
+                    <div class="row">
+                    <div class="card mb-4">
+                    <div class="card-header">
+                        <i class="fas fa-table me-1"></i>
+                        Usuarios
+                    </div>
+                    <div class="card-body">
+                        <table id="datatablesSimple">
+                            <thead>
+                                <tr>
+                                <th>ID</th>
+                                <th>Username</th>
+                                <th>Contraseña</th>
+                                <th>Tipo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                              foreach ($data1 as $usuario) {
+                                echo("<tr>");
+                                echo("<td>$usuario[0]</td>");
+                                echo("<td>$usuario[1]</td>");
+                                echo("<td>$usuario[2]</td>");
+                                echo("<td>$usuario[3]</td>");
+                                echo("</tr>");
+                              }
+                            ?>
+                            </tbody>
+                        </table>
+                    </div>
+                    </div>
+                    </div>
 
 <?php include('../templates/footer.php') ?>
