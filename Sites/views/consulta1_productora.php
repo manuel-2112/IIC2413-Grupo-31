3b642f7dd03ba1f9235560e8bdc6271cf087b53f
Felipe Aguirre <felipe.aguirre@uc.cl>
2022-12-02 22:49:42 -0300

    dnggg

--- Sites/views/consulta1_productora.php
+++ Sites/views/consulta1_productora.php
@@ -81,7 +81,7 @@
                         #Llama a conexión, crea el objeto PDO y obtiene la variable $db
                         require("../config/connection.php");
 
-                        $query = "SELECT E.nombre as nombre, E.fecha_inicio as fecha_inicio, E.fecha_termino
+                        $query = "SELECT E.nombre as nombre, E.fecha_inicio as fecha_inicio, E.fecha_termino, E.estado
                         FROM Evento as E, Productoras as P, ProductoraEvento as PE
                         WHERE P.id_p = PE.id_p AND E.id_ev = PE.id_ev AND LOWER(P.nombre) = '$name'";
                         $result = $db2 -> prepare($query);
@@ -99,12 +99,13 @@
                             <thead>
                                 <tr>
                                     <th>Nombre Productoras</th>
+                                    <th>Estado</th>
                                 </tr>
                             </thead>
                             <tbody>
                             <?php
                                 foreach ($productoras as $productora) {
-                                    echo "<tr> <td>$productora[0]</td> </tr>";
+                                    echo "<tr> <td>$productora[0]</td><td>$productora[3]</td> </tr>";
                                 }
                             ?>
                             </tbody>
